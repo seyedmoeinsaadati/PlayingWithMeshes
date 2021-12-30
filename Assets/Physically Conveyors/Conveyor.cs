122443d96d96807f7926bb025cc59391f5b5fb1b
msaadati <s.saadati@tapsell.ir>
2021-12-30 10:15:53 +0330

    -- Conveyor add core mechanic

---
+++ Assets/Physically Conveyors/Conveyor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Conveyor : MonoBehaviour
+{
+    public bool active = true;
+    public float speed;
+    public Vector3 direction = Vector3.back;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (active)
+        {
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        Vector3 pos = rb.position;
+        rb.position += direction.normalized * (speed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+    }
+}
