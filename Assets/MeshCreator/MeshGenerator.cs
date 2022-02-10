2adf32992aae8546d99a48662019787c969e007c
seyedmoeinsaadati <saadatimoin@gmail.com>
2022-02-10 14:51:16 +0330

    add MeshPreview.cs organize files fix flat, smooth calcualtions

--- Assets/MeshCreator/MeshGenerator.cs
+++ Assets/MeshCreator/MeshGenerator.cs
@@ -2,36 +2,6 @@
 
 public static class MeshGenerator
 {
-    // public bool autoUpdate;
-    //
-    // [SerializeField] private MeshFilter viewMeshFilter;
-    // private Mesh viewMesh;
-    //
-    //
-    // [Header("Mesh Setting:")] public AnimationCurve graph;
-    //
-    // [Min(1)] public int resolution = 2;
-    // [Min(3)] public int side;
-    //
-    // // [Range(-1, 1)] public float offset = 0;
-    // public float width = 1;
-    // public float height = 1;
-
-    // public void Generate()
-    // {
-    //     if (viewMeshFilter == null || viewMesh == null)
-    //     {
-    //         viewMeshFilter = GetComponent<MeshFilter>();
-    //
-    //         viewMesh = new Mesh {name = "View Mesh"};
-    //         viewMeshFilter.mesh = viewMesh;
-    //     }
-    //
-    //     CreateCylinder();
-    // }
-
-    // private Vector3[] vertices;
-
     /// <summary>
     /// create a cylinder
     /// </summary>
@@ -52,9 +22,9 @@ public static class MeshGenerator
 
         // initialize mesh data
         Vector3[] vertices = new Vector3[vertexCount];
-        int[] triangles = new int[triangleCount * 3];
+        Vector3[] vertexNormals = new Vector3[vertexCount];
         Vector2[] uvs = new Vector2[vertexCount];
-        Vector3[] vertexNormals = new Vector3[vertices.Length];
+        int[] triangles = new int[triangleCount * 3];
 
         // creating...
         // vertex, uvs calculation
@@ -131,17 +101,21 @@ public static class MeshGenerator
 
             vertices = flatShadedVertices;
             uvs = flatShadedUVs;
+        }
 
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+
+        if (normalMode == NormalMode.Flat)
+        {
             mesh.RecalculateNormals();
         }
         else if (normalMode == NormalMode.Smooth)
         {
             mesh.normals = vertexNormals;
         }
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.uv = uvs;
 
         return mesh;
     }
@@ -151,79 +125,4 @@ public static class MeshGenerator
         Flat = 0,
         Smooth = 1
     }
-
-
-    // public static Mesh CreateCylinderCurve(float radius, float length, int side, int segment, AnimationCurve graph,
-    //     string name = "Default")
-    // {
-    //     // create cylinder with open top and down
-    //     int vertexCount = (side + 1) * (segment + 1);
-    //     int triangleCount = 2 * segment * side;
-    //
-    //     Vector3[] vertices = new Vector3[vertexCount];
-    //     int[] triangles = new int[triangleCount * 3];
-    //
-    //     float angleStep = (float) 360 / side;
-    //     float step = (float) 1 / segment;
-    //
-    //     for (int i = 0; i < segment + 1; i++)
-    //     {
-    //         float anglePointer = 0;
-    //         for (int j = 0; j < side + 1; j++)
-    //         {
-    //             var vertexIndex = i * (side + 1) + j;
-    //             Vector3 vertexPos = new Vector3(
-    //                 Mathf.Cos(anglePointer * Mathf.Deg2Rad) * radius,
-    //                 (float) i / segment * length,
-    //                 Mathf.Sin(anglePointer * Mathf.Deg2Rad) * radius);
-    //
-    //             vertices[vertexIndex] = vertexPos;
-    //
-    //             anglePointer += angleStep;
-    //         }
-    //     }
-    //
-    //     for (int ti = 0, vi = 0, y = 0; y < segment; y++, vi++)
-    //     {
-    //         for (int x = 0; x < side; x++, ti += 6, vi++)
-    //         {
-    //             triangles[ti] = vi;
-    //             triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-    //             triangles[ti + 4] = triangles[ti + 1] = vi + side + 1;
-    //             triangles[ti + 5] = vi + side + 2;
-    //         }
-    //     }
-    //
-    //     Mesh mesh = new Mesh
-    //     {
-    //         name = name,
-    //         vertices = vertices,
-    //         triangles = triangles
-    //     };
-    //     mesh.RecalculateNormals();
-    //
-    //     return mesh;
-    // }
-
-    // private void OnDrawGizmos()
-    // {
-    //     Gizmos.color = Color.black;
-    //     for (int i = 0; i < vertices.Length; i++)
-    //     {
-    //         // if (vertices[i] == Vector3.zero)
-    //         // {
-    //         //     print(i);
-    //         // }
-    //
-    //         Gizmos.DrawSphere(vertices[i], 0.01f);
-    //     }
-    // }
-    //
-    // private void OnValidate()
-    // {
-    //     if (autoUpdate)
-    //     {
-    //         Generate();
-    //     }
-    // }
 }
---
+++ Assets/MeshPreview.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class MeshPreview : MonoBehaviour
+{
+    public enum MeshType
+    {
+        Cylinder,
+        CylinderWithGraph
+    }
+
+    public bool autoUpdate;
+    public MeshType type;
+
+    [SerializeField] private MeshFilter viewMeshFilter;
+    private Mesh viewMesh;
+
+    [Header("Mesh Setting:")] public AnimationCurve graph;
+    [Min(1)] public int segment = 2;
+    [Min(3)] public int side;
+    public MeshGenerator.NormalMode normalMode;
+
+    // [Range(-1, 1)] public float offset = 0;
+    public float radius = 1;
+    public float length = 1;
+
+    public void DrawMesh(Mesh mesh)
+    {
+        viewMeshFilter.sharedMesh = mesh;
+
+        // textureRenderer.gameObject.SetActive(false);
+        // meshFilter.gameObject.SetActive(true);
+    }
+
+    public void DrawInEditor()
+    {
+        switch (type)
+        {
+            case MeshType.Cylinder:
+                DrawMesh(MeshGenerator.CreateCylinder(radius, length, side, segment, normalMode, "preview_cylinder"));
+                break;
+            case MeshType.CylinderWithGraph:
+                // DrawMesh(MeshGenerator.CreateCylinder());
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(MeshPreview))]
+public class MeshPreviewEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MeshPreview meshPreview = (MeshPreview) target;
+
+        DrawDefaultInspector();
+
+        if (meshPreview.autoUpdate)
+        {
+            meshPreview.DrawInEditor();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            meshPreview.DrawInEditor();
+        }
+
+        if (GUILayout.Button("Save Mesh as FBX"))
+        {
+            // meshPreview.SaveMesh();
+        }
+    }
+}
+#endif
